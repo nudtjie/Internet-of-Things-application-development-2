e5b8df9f5aa01d2c21cc72f427fb46c0834e7182
Jerry <33362938+nudtjie@users.noreply.github.com>
2019-03-14 18:19:17 +0800

    Add files via upload

---
+++ chuankou.ino
@@ -0,0 +1,22 @@
+#include<SoftwareSerial.h>
+SoftwareSerial BluetoothSerial(8,9);//3A：46 ，9：RX
+char value;
+void setup(){
+  Serial.begin(38400);
+  Serial.println("Serial Connected!");
+  BluetoothSerial.begin(38400);
+}
+
+void loop(){
+  if(Serial.available()){
+    value = Serial.read();
+    //Serial.print(value);
+    BluetoothSerial.write(value);
+  }
+  if(BluetoothSerial.available()){
+    value = BluetoothSerial.read();
+    Serial.print(value);
+    //Serial.println("123456");
+  }
+ 
+}
